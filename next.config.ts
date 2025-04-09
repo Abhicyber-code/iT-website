1fbc52bd95d4aca157492db63bf568c0afdd9097
Abhijeet Gitte <74947227+Abhicyber-code@users.noreply.github.com>
2025-04-09 10:50:08 +0530

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,15 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    domains: [
+      'randomuser.me',
+      'encrypted-tbn0.gstatic.com',
+      'jspmjscoe.edu.in',
+      'images.unsplash.com',
+      'cdn.jsdelivr.net'
+    ],
+  },
+};
+
+export default nextConfig;
